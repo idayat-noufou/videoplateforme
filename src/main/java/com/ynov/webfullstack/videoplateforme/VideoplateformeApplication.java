9d7ad46477dcd36a5f8a01675001cab41146d034
Jamil18474 <jamil.abdelhamid@ynov.com>
2024-03-28 11:32:12 +0100

    spring projet

---
+++ src/main/java/com/ynov/webfullstack/videoplateforme/VideoplateformeApplication.java
@@ -0,0 +1,13 @@
+package com.ynov.webfullstack.videoplateforme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoplateformeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoplateformeApplication.class, args);
+    }
+
+}
