9a5ddad4d2226e90d5c1e31ce0d4b757ceba6ff5
Idayat Noufou <idayat.noufou@ynov.com>
2024-03-28 11:32:12 +0100

    spring projet

---
+++ src/test/java/com/ynov/webfullstack/videoplateforme/VideoplateformeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynov.webfullstack.videoplateforme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoplateformeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
